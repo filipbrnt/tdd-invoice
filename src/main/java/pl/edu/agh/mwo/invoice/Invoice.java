fe77ad6878f59b85e5565047fffed74b72f860e5
filipbrnt <filipbiernat@gmail.com>
2019-02-09 11:57:24 +0100

    Implement all Invoice tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,53 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
 	private Collection<Product> products;
+		
+	public Invoice() {
+		products = new ArrayList<>();
+	}
 
 	public void addProduct(Product product) {
-		// TODO: implement
+		products.add(product);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (quantity <= 0) {
+			throw new IllegalArgumentException();
+		}
+		for (int i = 0; i < quantity; i++) {
+			addProduct(product);
+		}
 	}
 
 	public BigDecimal getSubtotal() {
-		return null;
+		BigDecimal subtotal = new BigDecimal("0");
+		if (!products.isEmpty())
+			for (Product product : products) {
+				subtotal = subtotal.add(product.getPrice());
+			}			
+		return subtotal;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		BigDecimal tax = BigDecimal.ZERO;
+		if (!products.isEmpty())
+			for (Product product : products) {
+				tax = tax.add(product.getPrice().multiply(product.getTaxPercent()));
+			}
+		return tax;
 	}
 
 	public BigDecimal getTotal() {
-		return null;
+		if (products.isEmpty()) {
+			return BigDecimal.ZERO;			
+		}
+		return this.getSubtotal().add(this.getTax());
 	}
 }
